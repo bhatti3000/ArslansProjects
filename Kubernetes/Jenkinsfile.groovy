641b88c56348e9d4a27335f28a400d25494ddcd0
unknown <m_a_majid88@yahoo.com>
2025-10-04 19:15:10 +0200

    Update on 2025-10-04 19:15:10

--- Kubernetes/Jenkinsfile.groovy
+++ Kubernetes/Jenkinsfile.groovy
@@ -1,6 +1,11 @@
 pipeline {
   agent any
+  options { timestamps() }
+
   stages {
+    stage('Checkout') {
+      steps { checkout scm }
+    }
     stage('Hello') {
       steps { echo 'Jenkins is running on the VM 🎉' }
     }
